701a23b79c1d76bd3625cae44f8f00f84288e512
Vincent <vshan0816@gmail.com>
2023-08-08 14:41:07 -0400

    Finish application controller

--- practice-for-ch-rails-music-app-long-practice-main/music-app/app/controllers/sessions_controller.rb
+++ practice-for-ch-rails-music-app-long-practice-main/music-app/app/controllers/sessions_controller.rb
@@ -1,3 +1,26 @@
 class SessionsController < ApplicationController
+    def new
+        user = User.new
+        render :new
+    end
 
+    def create
+        @user = user.find_by_credentials(
+            params[:user][:email]
+            params[:user][:password]
+        )
+
+        if @user
+            login!(@user)
+            redirect_to user_url(@user.id)
+        else
+            @user = User.new(username: params[:user][:username])
+            render :new
+        end
+    end
+
+    def destroy
+        logout!
+        redirect_to new_session_url
+    end
 end
