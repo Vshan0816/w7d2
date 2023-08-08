169acc6ac6b5edc0f7fcb14eaf0ea115907faaae
Vincent <vshan0816@gmail.com>
2023-08-08 14:01:11 -0400

    Finish users model

---
+++ practice-for-ch-rails-music-app-long-practice-main/music-app/app/models/user.rb
@@ -0,0 +1,35 @@
+class User < ApplicationRecord
+
+    before_validation  :ensure_session_token
+    attr_reader: password
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+
+    def is_password?(password)
+        password_object = BCrypt::Password.new(self.password_digest)
+        password_object.is_password?(password)
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom::urlsafe_base64
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token
+    end
+end
